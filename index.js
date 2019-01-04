830874d9d310df79012e1711984a292cefc6b958
Project 33 <project33asap@gmail.com>
2019-01-04 13:01:37 +0530

    message

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('hello'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
